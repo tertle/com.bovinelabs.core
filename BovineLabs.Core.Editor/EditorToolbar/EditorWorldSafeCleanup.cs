6ea83d2ad7b8636eb5e50a73e1d831b61f78edc3
Tim (tertle) <tim@ninjatertle.com>
2022-12-21 07:48:26 +0000

    0.11

---
+++ BovineLabs.Core.Editor/EditorToolbar/EditorWorldSafeCleanup.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="EditorWorldSafeCleanup.cs" company="BovineLabs">
+//     Copyright (c) BovineLabs. All rights reserved.
+// </copyright>
+
+namespace BovineLabs.Core.Editor.EditorToolbar
+{
+    using Unity.Entities;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.LowLevel;
+
+    [InitializeOnLoad]
+    public static class EditorWorldSafeCleanup
+    {
+        static EditorWorldSafeCleanup()
+        {
+            Application.quitting += OnQuit;
+        }
+
+        private static void OnQuit()
+        {
+            var playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+            foreach (var w in World.s_AllWorlds)
+            {
+                ScriptBehaviourUpdateOrder.RemoveWorldFromPlayerLoop(w, ref playerLoop);
+            }
+
+            PlayerLoop.SetPlayerLoop(playerLoop);
+            World.DisposeAllWorlds();
+        }
+    }
+}
--- BovineLabs.Core.Editor/EditorToolbar/ToolbarCallback.cs
+++ BovineLabs.Core.Editor/EditorToolbar/ToolbarCallback.cs
@@ -12,23 +12,10 @@ namespace BovineLabs.Core.Editor.EditorToolbar
 
     public static class ToolbarCallback
     {
-        /// <summary>
-        /// Callback for toolbar OnGUI method.
-        /// </summary>
-        public static Action OnToolbarGUI;
+        public static Action OnToolbarGUILeft;
+        public static Action OnToolbarGUIRight;
 
         private static readonly Type ToolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-        private static readonly Type GUIViewType = typeof(Editor).Assembly.GetType("UnityEditor.GUIView");
-        private static readonly Type WindowBackendType = typeof(Editor).Assembly.GetType("UnityEditor.IWindowBackend");
-
-        private static readonly PropertyInfo WindowBackend =
-            GUIViewType.GetProperty("windowBackend", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-        private static readonly PropertyInfo ViewVisualTree =
-            WindowBackendType.GetProperty("visualTree", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-        private static readonly FieldInfo IMGUIContainerOnGui =
-            typeof(IMGUIContainer).GetField("m_OnGUIHandler", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
         private static ScriptableObject currentToolbar;
 
@@ -48,60 +35,30 @@ namespace BovineLabs.Core.Editor.EditorToolbar
                 currentToolbar = toolbars.Length > 0 ? (ScriptableObject)toolbars[0] : null;
                 if (currentToolbar != null)
                 {
-#if UNITY_2021_1_OR_NEWER
                     var root = currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
-                    var rawRoot = root.GetValue(currentToolbar);
-                    var mRoot = rawRoot as VisualElement;
-                    RegisterCallback("ToolbarZoneLeftAlign", OnToolbarGUILeft);
-                    RegisterCallback("ToolbarZoneRightAlign", OnToolbarGUIRight);
-
-                    void RegisterCallback(string root, Action cb)
-                    {
-                        var toolbarZone = mRoot.Q(root);
-
-                        var parent = new VisualElement
-                        {
-                            style =
-                            {
-                                flexGrow = 1,
-                                flexDirection = FlexDirection.Row,
-                            },
-                        };
-                        var container = new IMGUIContainer();
-                        container.onGUIHandler += () => { cb?.Invoke(); };
-                        parent.Add(container);
-                        toolbarZone.Add(parent);
-                    }
-#else
-                    var windowBackend = WindowBackend.GetValue(currentToolbar);
-
-                    // Get it's visual tree
-                    var visualTree = (VisualElement)ViewVisualTree.GetValue(windowBackend, null);
-
-
-                    // Get first child which 'happens' to be toolbar IMGUIContainer
-                    var container = (IMGUIContainer)visualTree[0];
-
-                    // (Re)attach handler
-                    var handler = (Action)IMGUIContainerOnGui.GetValue(container);
-                    handler -= OnGUI;
-                    handler += OnGUI;
-                    IMGUIContainerOnGui.SetValue(container, handler);
-#endif
+                    var mRoot = (VisualElement)root!.GetValue(currentToolbar);
+                    RegisterCallback(mRoot, "ToolbarZoneLeftAlign", OnToolbarGUILeft);
+                    RegisterCallback(mRoot, "ToolbarZoneRightAlign", OnToolbarGUIRight);
                 }
             }
         }
 
-        private static void OnGUI()
+        private static void RegisterCallback(VisualElement elementRoot, string root, Action cb)
         {
-            var handler = OnToolbarGUI;
-            handler?.Invoke();
+            var toolbarZone = elementRoot.Q(root);
+
+            var parent = new VisualElement
+            {
+                style =
+                {
+                    flexGrow = 1,
+                    flexDirection = FlexDirection.Row,
+                },
+            };
+            var container = new IMGUIContainer();
+            container.onGUIHandler += () => { cb?.Invoke(); };
+            parent.Add(container);
+            toolbarZone.Add(parent);
         }
-
-#if UNITY_2021_1_OR_NEWER
-        public static Action OnToolbarGUILeft;
-
-        public static Action OnToolbarGUIRight;
-#endif
     }
 }
