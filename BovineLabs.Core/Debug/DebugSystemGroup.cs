b1c652c2dbef3fcad0c8af96c8583fc2666306a0
Tim <6189147+tertle@users.noreply.github.com>
2026-09-20 12:02:09 +1000

    docs: explain system group purposes

--- BovineLabs.Core/Debug/DebugSystemGroup.cs
+++ BovineLabs.Core/Debug/DebugSystemGroup.cs
@@ -3,6 +3,9 @@
     using Unity.Entities;
     using WorldFlag = Unity.Entities.WorldSystemFilterFlags;
 
+    /// <summary>
+    /// Groups diagnostic systems in presentation across supported simulation and editor worlds.
+    /// </summary>
     [UpdateInGroup(typeof(PresentationSystemGroup))]
     [WorldSystemFilter(WorldFlag.LocalSimulation | WorldFlag.ClientSimulation | WorldFlag.ServerSimulation | WorldFlag.ThinClientSimulation | WorldFlag.Editor,
         WorldFlag.LocalSimulation | WorldFlag.ClientSimulation | WorldFlag.ServerSimulation)]
--- BovineLabs.Core/Internal/BeforeSceneSectionStreamingSystemGroup.cs
+++ BovineLabs.Core/Internal/BeforeSceneSectionStreamingSystemGroup.cs
@@ -3,6 +3,9 @@
     using Unity.Entities;
     using Unity.Scenes;
 
+    /// <summary>
+    /// Provides a hook after scene references resolve but before scene sections start streaming.
+    /// </summary>
     [UpdateBefore(typeof(SceneSectionStreamingSystem))]
     [UpdateAfter(typeof(ResolveSceneReferenceSystem))]
     [UpdateInGroup(typeof(SceneSystemGroup))]
